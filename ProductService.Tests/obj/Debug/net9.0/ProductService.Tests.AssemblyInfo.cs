0f022cb55abfdd5e9953e652de9725101960564d
Alper Yusuf Dursun <AlperYusuf.Dursun@aydinli.com.tr>
2025-07-03 14:06:15 +0300

    Initial commit - ProductService microservice project

--- ProductService.Tests/obj/Debug/net9.0/ProductService.Tests.AssemblyInfo.cs
+++ ProductService.Tests/obj/Debug/net9.0/ProductService.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductService.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef82a0c41e06be90c60d69bdbb6139b8c184575")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductService.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductService.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
